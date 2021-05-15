6ac1099f2b37de43aa058209b5d555259aa16d3c
Max Clarke <maximilian-jc@live.com>
2021-05-15 15:17:32 +0100

    02 completed

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 /**
  * Make the following POST request with either axios or node-fetch:
 
@@ -28,8 +29,24 @@ The results should have this structure:
  *  the "name" of the package that has the oldest "date" value
  */
 
-module.exports = async function oldestPackageName() {
-  // TODO
 
-  return name
+module.exports = async function oldestPackageName() {
+
+  return axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
+   url: 'https://api.npms.io/v2/search/suggestions?q=react',
+   method: "GET",
+   return_payload: true
+ })
+ .then((response) => {
+    arr = [];
+    for(i=0;i<response.data.content.length;i++) {
+      arr.push(response.data.content[i].package)
+    }
+
+   oldestPackage = arr.reduce((r, o) => r.date > o.date ? o : r);
+   let name = oldestPackage.name
+   return name
+ }, (error) => {
+   console.log(error);
+ });
 };
